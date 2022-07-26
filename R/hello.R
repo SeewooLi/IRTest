d3fd6d92fca1f28350613baee9a95183a9b19f74
CU <seewoo1107@naver.com>
2022-07-26 17:02:07 +0900

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' title
+#' @param x as
+#' @description a function
+#' @return the output from \code{\link{print}}
+#'
+#' @export
+#'
+#' @examples
+#' hello("Seewoo")
+#' \dontrun{
+#' hello("NAGAP")
+#' }
+hello <- function(x) {
+  print("Hello, world!")
+}
