6afc92b293986ec4105ea42c6dfdcda3460d3742
CU <seewoo1107@naver.com>
2022-08-06 23:51:43 +0900

    pdfs and likelihood for GPCM

--- R/Functions_IRTest.R
+++ R/Functions_IRTest.R
@@ -1,6 +1,3 @@
-source("Davidian curve.R")
-if (!"dcurver" %in% installed.packages()) install.packages("dcurver")
-if (!"betafunctions" %in% installed.packages()) install.packages("betafunctions")
 #################################################################################################################
 # ICC
 #################################################################################################################
@@ -8,14 +5,24 @@ P <- function(theta,a=1,b,c=0){
   c+(1-c)*(1/(1+exp(-a*(theta-b))))
 }
 
+P_P <- function(theta, a, b){
+  if(is.vector(b)){
+    ps <- c(1,exp(a*cumsum(theta-b)))
+    ps <- ps/sum(ps)
+  }else{
+    ps <- cbind(1,exp(t(apply(a*(theta-b),1,cumsum))))
+    ps <- ps/rowSums(ps, na.rm = T)
+  }
+  return(ps)
+}
 
 #################################################################################################################
 # Likelihood
 #################################################################################################################
-logLikeli <- function(item, data, theta, model){
+logLikeli <- function(item, data, theta){
   if(length(theta)!=1){
     # if all of the examinees' ability values are specified
-    L <- matrix(nrow = nrow(data), ncol = ncol(data))  
+    L <- matrix(nrow = nrow(data), ncol = ncol(data))
       for(i in 1:ncol(data)){
         for(j in 1:nrow(data)){
           L[j,i] <- data[j,i]*log(P(theta = theta[j],a=item[i,1],b=item[i,2]))+(1-data[j,i])*log(1-P(theta = theta[j],a=item[i,1],b=item[i,2]))
@@ -23,17 +30,27 @@ logLikeli <- function(item, data, theta, model){
     }
   }else{
     # if a single ability value is assumed for all
-    L <- matrix(nrow = nrow(data), ncol = ncol(data))  
     pvector <- t(P(theta = theta,a=item[,1],b=item[,2],c=item[,3]))
-    
+
     lp1 <- log(pvector)
     lp2 <- log(1-pvector)
     lp1[lp1==-Inf] <- -.Machine$double.xmax # to avoid NaN
     lp2[lp2==-Inf] <- -.Machine$double.xmax # to avoid NaN
-    
+
     L <- tcrossprod(data, lp1)+tcrossprod((1-data), lp2)
     # L is an N times 1 vector, each element of which refers to an individual's log-likelihood
   }
+  return(L)
+}
+
+logLikeli_Poly <- function(item, data, theta){
+  pmat <- P_P(theta = theta, a = item[,1], b = item[,-1])
+  N <- nrow(data)
+  n <- ncol(data)
+  L <- log(matrix(pmat[matrix(cbind(rep(1:n, each = N),as.vector(data)),ncol=2)], nrow = N, ncol = n))
+  L[L==-Inf] <- -.Machine$double.xmax
+  L <- rowSums(L)
+  L[L==-Inf] <- -.Machine$double.xmax
   return(L)
 }
 
@@ -44,14 +61,14 @@ library(betafunctions)
 DataGeneration <- function(seed=1, N=2000, nitem=10, prob=0.5, d=1.7,
                            sd_ratio=1, a_l=0.8, a_u=2.5, latent_dist="normal",
                            model=3, c_l=0, c_u=0.2){
-  
+
   if(model==2){
     set.seed(seed)
     item <- matrix(c(runif(nitem,a_l,a_u),
                      sample(seq(-2,2,by=0.01),nitem, prob = dnorm(seq(-2,2,by=0.01)))), ncol=2)
     item <- round(item, digits = 2)
     initialitem <- matrix(c(rep((a_l+a_u)/2,nitem),
-                            rep(0,2*nitem)), ncol=3)    
+                            rep(0,2*nitem)), ncol=3)
   } else if(model==3){
     set.seed(seed)
     item <- matrix(c(runif(nitem,a_l,a_u),
@@ -61,7 +78,7 @@ DataGeneration <- function(seed=1, N=2000, nitem=10, prob=0.5, d=1.7,
     initialitem <- matrix(c(rep((a_l+a_u)/2,nitem),
                             rep(0,2*nitem)), ncol=3)
   }
-  
+
   if(latent_dist=="beta"){
     set.seed(seed)
     theta <- rBeta.4P(n=N, alpha = 3.79, beta= 10.21, l=-2.36, u=6.36)
@@ -75,12 +92,12 @@ DataGeneration <- function(seed=1, N=2000, nitem=10, prob=0.5, d=1.7,
     m2 <- prob*d
     s1 <- sqrt((1-prob*(1-prob)*d^2)/(prob+(1-prob)*sd_ratio^2))
     s2 <- s1*sd_ratio
-    
+
     set.seed(seed)
     theta <- c(rnorm(n=n1,mean=m1,sd=s1),rnorm(n=n2,mean=m2,sd=s2))
   } else stop("Specify type of theta dist.")
   data <- matrix(nrow = N, ncol = nitem)
-  
+
   if(model==2){
     set.seed(seed)
     for(i in 1:nitem){
@@ -178,7 +195,7 @@ M1step <- function(E, item, model, max_iter=10, threshold=1e-7, EMiter){
   ####item parameter estimation####
   for(i in 1:nitem){
     if(model[i] %in% c(1, "1PL", "Rasch", "RASCH")){
-      
+
       iter <- 0
       div <- 3
       par <- item[i,2]
@@ -188,7 +205,7 @@ M1step <- function(E, item, model, max_iter=10, threshold=1e-7, EMiter){
         p <- P(theta = X, b=par)
         fW <- f*p*(1-p)
         diff <- as.vector(sum(r[i,]-f*p)/sum(fW))
-        
+
         if(is.infinite(sum(abs(diff)))|is.na(sum(abs(diff)))){
           par <- par
         } else{
@@ -203,9 +220,9 @@ M1step <- function(E, item, model, max_iter=10, threshold=1e-7, EMiter){
       }
       item_estimated[i,2] <- par
       se[i,2] <- sqrt(1/sum(fW)) # asymptotic S.E.
-    
+
     } else if(model[i] %in% c(2, "2PL")){
-    
+
     iter <- 0
     div <- 3
     par <- item[i,c(1,2)]
@@ -237,9 +254,9 @@ M1step <- function(E, item, model, max_iter=10, threshold=1e-7, EMiter){
     }
     item_estimated[i,c(1,2)] <- par
     se[i,c(1,2)] <- sqrt(-c(inv_L2[1,1], inv_L2[2,2])) # asymptotic S.E.
-  
+
     } else if(model[i] %in% c(3, "3PL")){
-      
+
       iter <- 0
       div <- 3
       par <- item[i,c(1,2,3)]
@@ -254,15 +271,15 @@ M1step <- function(E, item, model, max_iter=10, threshold=1e-7, EMiter){
         par[1] <- max(0.1,par[1])
         par[3] <- max(0,par[3])
         X_ <- X-par[2]
-        
+
         L1 <- c((1-par[3])*sum(X_*(r[i,]-fp)*W_),
                 -par[1]*(1-par[3])*sum((r[i,]-fp)*W_),
                 sum(r[i,]/p-f))/(1-par[3]) #1st derivative of marginal likelihood
-        
+
         L2 <- diag(c(sum(-X_^2*fW*(p_/p)^2), #2nd derivative of marginal likelihood
                      -par[1]^2*sum(fW*(p_/p)^2),
                      -sum(fW/(p^2))/(1-par[3])^2
-                     )) 
+                     ))
         L2[2,1] <- par[1]*sum(X_*fW*(p_/p)^2); L2[1,2] <- L2[2,1]
         L2[3,1] <- -sum(X_*fW*p_/p^2)/(1-par[3]); L2[1,3] <- L2[3,1]
         L2[3,2] <- par[1]*sum(fW*p_/p^2)/(1-par[3]); L2[2,3] <- L2[3,2]
@@ -284,7 +301,7 @@ M1step <- function(E, item, model, max_iter=10, threshold=1e-7, EMiter){
       item_estimated[i,c(1,2,3)] <- par
       se[i,c(1,2,3)] <- sqrt(-c(inv_L2[1,1], inv_L2[2,2], inv_L2[3,3])) # asymptotic S.E.
     } else warning("model is incorrect or unspecified.")
-  
+
   }
   return(list(item_estimated, se))
 }
@@ -305,7 +322,7 @@ M2step <- function(E, max_iter=200){
   # EM algorithm - Gaussian Mixture
   repeat{
     iter <- iter+1
-    
+
     resp1 <- f*(prob*dnormal(X,m1,s1))/
       (prob*dnormal(X,m1,s1)+(1-prob)*dnormal(X,m2,s2)) # responsibility
     resp2 <- f- resp1
@@ -353,16 +370,16 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
   iter <- 0
   diff <- 1
   prob = 0.5
-  d = 1 
+  d = 1
   sd_ratio = 1
   N = nrow(data)
   bw <- NULL
-  
+
   # Normality assumption method
   if(latent_dist %in% c("Normal", "normal", "N")){
     while(iter < max_iter & diff > threshold){
       iter <- iter +1
-      
+
       E <- Estep(item=initialitem, data=data, q=q, prob=0.5, d=0, sd_ratio=1, range=range)
       M1 <- M1step(E, item=initialitem, model=model)
       initialitem <- M1[[1]]
@@ -373,22 +390,22 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
     }
     Ak <- E$Ak
   }
-  
+
   # Empirical histogram method
   if(latent_dist=="EHM"){
     while(iter < max_iter & diff > threshold){
       iter <- iter +1
-      
+
       E <- Estep(item=initialitem, data=data, q=q, prob=0.5, d=0, sd_ratio=1, range=range, Xk=Xk, Ak=Ak)
       M1 <- M1step(E, item=initialitem, model=model)
       initialitem <- M1[[1]]
-      
+
       post_den <- E$fk/sum(E$fk)
       Xk <- E$Xk
       lin <- lin_inex(Xk, post_den, range = range)
       Xk <- lin$qp
       post_den <- lin$qh
-      
+
       diff <- max(abs(I-initialitem), na.rm = T)
       I <- initialitem
       Ak <- post_den
@@ -396,12 +413,12 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
       flush.console()
     }
   }
-  
+
   # Two-component normal mixture distribution
   if(latent_dist=="Mixture"){
     while(iter < max_iter & diff > threshold){
       iter <- iter +1
-      
+
       E <- Estep(item=initialitem, data=data, q=q, prob=prob, d=d, sd_ratio=sd_ratio, range = range)
       M1 <- M1step(E, item=initialitem, model=model)
       initialitem <- M1[[1]]
@@ -414,24 +431,24 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
     }
     Ak <- E$Ak
   }
-  
+
   # Kernel density estimation method
   if(latent_dist=="KDE"){
     while(iter < max_iter & diff > threshold){
       iter <- iter +1
-      
-      E <- Estep(item=initialitem, data=data, q=q, prob=0.5, d=0, sd_ratio=1, 
+
+      E <- Estep(item=initialitem, data=data, q=q, prob=0.5, d=0, sd_ratio=1,
                  range=range, Xk=Xk, Ak=Ak)
       M1 <- M1step(E, item=initialitem, model=model)
       initialitem <- M1[[1]]
-      
+
       post_den <- E$fk/sum(E$fk)
       Xk <- E$Xk
       post_den <- lin_inex(Xk, post_den, range = range)$qh
       nzindex <- round(post_den*N)!=0
       SJPI <- density(rep(Xk[nzindex], times=round(post_den*N)[nzindex]), bw = bandwidth,n=q, from = range[1], to=range[2])
       post_den <- lin_inex(Xk, SJPI$y/sum(SJPI$y), range = range)$qh
-      
+
       diff <- max(abs(I-initialitem), na.rm = T)
       I <- initialitem
       Ak <- post_den
@@ -440,30 +457,30 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
     }
     bw <- c(SJPI$bw, SJPI$n)
   }
-  
+
   # Davidian curve method
   if(latent_dist=="DC"){
-    phipar <- nlminb(start = rep(1,h), 
+    phipar <- nlminb(start = rep(1,h),
                      objective = optim_phi,
                      gradient = optim_phi_grad,
                      hp=h,
-                     lower = -pi/2, 
+                     lower = -pi/2,
                      upper = pi/2)$par
-    
+
     while(iter < max_iter & diff > threshold){
       iter <- iter +1
-      
+
       E <- Estep(item=initialitem, data=data, q=q, range=range, Xk=Xk, Ak=Ak)
       M1 <- M1step(E, item=initialitem, model = model)
       initialitem <- M1[[1]]
-      
+
       Xk <- E$Xk
       phipar <- nlminb(start = phipar,
                 objective = DC.LL,
                 gradient = DC.grad,
                 theta=E$Xk,
                 freq = E$fk)$par
-      
+
       post_den <- dcurver::ddc(x = Xk, phi = phipar)
       post_den <- post_den/sum(post_den)
       lin <- lin_inex(Xk, post_den, range = range, rule = 2)
@@ -477,7 +494,7 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
       flush.console()
     }
   }
-  
+
   # preparation for outputs
   EAP <- as.numeric(E$Pk%*%E$Xk)
   logL <- 0
@@ -486,16 +503,16 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
   }
   E$Pk[E$Pk==0]<- .Machine$double.xmin
   Ak[Ak==0] <- .Machine$double.xmin
-  logL <- logL + as.numeric(E$fk%*%log(Ak)) - sum(E$Pk*log(E$Pk)) 
-  return(list(par_est=initialitem, 
+  logL <- logL + as.numeric(E$fk%*%log(Ak)) - sum(E$Pk*log(E$Pk))
+  return(list(par_est=initialitem,
               se=M1[[2]],
-              fk=E$fk, 
-              iter=iter, 
-              prob=prob, 
-              d=d, 
-              sd_ratio=sd_ratio, 
-              quad=Xk, 
-              diff=diff, 
+              fk=E$fk,
+              iter=iter,
+              prob=prob,
+              d=d,
+              sd_ratio=sd_ratio,
+              quad=Xk,
+              diff=diff,
               Ak=Ak,
               Pk=E$Pk,
               theta = EAP,
@@ -510,14 +527,14 @@ IRTest <- function(initialitem, data, range = c(-6,6), q = 121, model,
 #################################################################################################################
 plot_LD <- function(model, from = -6, to=6, add = F){
   if(model[["Options"]][["latent_dist"]]=="Mixture"){
-    curve(dist2(x,prob = model$prob, d=model$d, sd_ratio = model$sd_ratio), 
+    curve(dist2(x,prob = model$prob, d=model$d, sd_ratio = model$sd_ratio),
           from = from, to=to, add = add, ylab="latent density", xlab=expression(theta))
   } else {
     if(model[["Options"]][["latent_dist"]]=="Normal"){
-      curve(dnormal(x), 
+      curve(dnormal(x),
             from = from, to=to, add = add, ylab="latent density", xlab=expression(theta))
     }else{
-      plot(model$quad,model$Ak*(1/(model$quad[2]-model$quad[1])), 
+      plot(model$quad,model$Ak*(1/(model$quad[2]-model$quad[1])),
            type = "l", ylab="latent density", xlab=expression(theta))
     }
   }
