6ef109bc62d37b1f931a2ba169107b66d182a3a6
seewoo1107@naver.com <seewoo1107@naver.com>
2023-03-29 23:53:14 +0900

    initial theta value available

--- R/DataGeneration.R
+++ R/DataGeneration.R
@@ -113,70 +113,79 @@
 #' initialitemP <- Alldata$initialitem_P
 #' theta <- Alldata$theta
 #'
-DataGeneration <- function(seed=1, N=2000, nitem_D=NULL, nitem_P=NULL, model_D, model_P="GPCM",
+DataGeneration <- function(seed=1, N=2000,
+                           item_D=NULL,
+                           item_P=NULL,
+                           theta = NULL,
+                           nitem_D=NULL, nitem_P=NULL,
+                           model_D, model_P="GPCM",
                            latent_dist="Mixture",
                            prob=0.5, d=1.7, sd_ratio=1, a_l=0.8, a_u=2.5,
                            c_l=0, c_u=0.2, categ){
-  item_D=NULL; initialitem_D=NULL; data_D=NULL
-  item_P=NULL; initialitem_P=NULL; data_P=NULL
+  initialitem_D=NULL; data_D=NULL
+  initialitem_P=NULL; data_P=NULL
 
 
 
   # ability parameters (i.e., theta)
+  if(is.null(theta)){
+    if(latent_dist=="beta"){
+      set.seed(seed)
+      theta <- rBeta.4P(n=N, alpha = 3.79, beta= 10.21, l=-2.36, u=6.36)
+    }else if(latent_dist=="chi"){
+      set.seed(seed)
+      theta <- scale(rchisq(N,df=8))
+    }else if(latent_dist=="Mixture"){
+      n1 <- round(N*prob)
+      n2 <- N-n1
+      m1 <- -(1-prob)*d
+      m2 <- prob*d
+      s1 <- sqrt((1-prob*(1-prob)*d^2)/(prob+(1-prob)*sd_ratio^2))
+      s2 <- s1*sd_ratio
 
-  if(latent_dist=="beta"){
-    set.seed(seed)
-    theta <- rBeta.4P(n=N, alpha = 3.79, beta= 10.21, l=-2.36, u=6.36)
-  }else if(latent_dist=="chi"){
-    set.seed(seed)
-    theta <- scale(rchisq(N,df=8))
-  }else if(latent_dist=="Mixture"){
-    n1 <- round(N*prob)
-    n2 <- N-n1
-    m1 <- -(1-prob)*d
-    m2 <- prob*d
-    s1 <- sqrt((1-prob*(1-prob)*d^2)/(prob+(1-prob)*sd_ratio^2))
-    s2 <- s1*sd_ratio
-
-    set.seed(seed)
-    theta <- c(rnorm(n=n1,mean=m1,sd=s1),rnorm(n=n2,mean=m2,sd=s2))
-  }else if(latent_dist=="normal"){
-    set.seed(seed)
-    theta <- rnorm(n=N,mean=0,sd=1)
-  } else stop("Specify the type of the latent distribution.")
-
+      set.seed(seed)
+      theta <- c(rnorm(n=n1,mean=m1,sd=s1),rnorm(n=n2,mean=m2,sd=s2))
+    }else if(latent_dist=="normal"){
+      set.seed(seed)
+      theta <- rnorm(n=N,mean=0,sd=1)
+    } else stop("Specify the type of the latent distribution.")
+  }
 
 
   # item parameters for dichotomous items
 
-  if(nitem_D!=0){
-    data_D <- matrix(nrow = N, ncol = nitem_D)
-    item_D <- matrix(nrow = nitem_D, ncol = 3)
-    initialitem_D <- matrix(nrow = nitem_D, ncol = 3)
-    set.seed(seed)
-    for(i in 1:nitem_D){
-      if(model_D[i]==1){
-        item_D[i,] <- round(c(1,
-                              sample(seq(-2,2,by=0.01),1, prob = dnorm(seq(-2,2,by=0.01))),
-                              0), digits = 2)
-        initialitem_D[i,] <- c(1,0,0)
-      }else if(model_D[i]==2){
-        item_D[i,] <- round(c(runif(1,a_l,a_u),
-                              sample(seq(-2,2,by=0.01),1, prob = dnorm(seq(-2,2,by=0.01))),
-                              0), digits = 2)
-        initialitem_D[i,] <- c((a_l+a_u)/2,0,0)
-      } else if(model_D[i]==3){
-        item_D[i,] <- round(c(runif(1,a_l,a_u),
-                              sample(seq(-2,2,by=0.01),1, prob = dnorm(seq(-2,2,by=0.01))),
-                              runif(1, min = c_l, max = c_u)), digits = 2)
-        initialitem_D[i,] <- c((a_l+a_u)/2,0,0)
-      }
+  if(is.null(theta)){
+    if(nitem_D!=0){
+      data_D <- matrix(nrow = N, ncol = nitem_D)
+      item_D <- matrix(nrow = nitem_D, ncol = 3)
+      initialitem_D <- matrix(nrow = nitem_D, ncol = 3)
+      set.seed(seed)
+      for(i in 1:nitem_D){
+        if(model_D[i]==1){
+          item_D[i,] <- round(c(1,
+                                sample(seq(-2,2,by=0.01),1, prob = dnorm(seq(-2,2,by=0.01))),
+                                0), digits = 2)
+          initialitem_D[i,] <- c(1,0,0)
+        }else if(model_D[i]==2){
+          item_D[i,] <- round(c(runif(1,a_l,a_u),
+                                sample(seq(-2,2,by=0.01),1, prob = dnorm(seq(-2,2,by=0.01))),
+                                0), digits = 2)
+          initialitem_D[i,] <- c((a_l+a_u)/2,0,0)
+        } else if(model_D[i]==3){
+          item_D[i,] <- round(c(runif(1,a_l,a_u),
+                                sample(seq(-2,2,by=0.01),1, prob = dnorm(seq(-2,2,by=0.01))),
+                                runif(1, min = c_l, max = c_u)), digits = 2)
+          initialitem_D[i,] <- c((a_l+a_u)/2,0,0)
+        }
+
+
 
       # item responses for dichotomous items
 
-      for(j in 1:N){
-        p <- P(theta = theta[j], a = item_D[i,1], b = item_D[i,2], c= item_D[i,3])
-        data_D[j,i] <- rbinom(1,1,prob = p)
+        for(j in 1:N){
+          p <- P(theta = theta[j], a = item_D[i,1], b = item_D[i,2], c= item_D[i,3])
+          data_D[j,i] <- rbinom(1,1,prob = p)
+        }
       }
     }
   }
