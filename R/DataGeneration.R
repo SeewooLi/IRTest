05e813dff7e8dabe2a6ea6007ca05a1c2ac9239e
seewoo1107@naver.com <seewoo1107@naver.com>
2023-03-30 00:31:53 +0900

    initial parameters available

--- R/DataGeneration.R
+++ R/DataGeneration.R
@@ -153,9 +153,8 @@ DataGeneration <- function(seed=1, N=2000,
 
 
   # item parameters for dichotomous items
-
-  if(is.null(theta)){
-    if(nitem_D!=0){
+  if(is.null(item_D)){
+    if((nitem_D!=0)&(!is.null(nitem_D))){
       data_D <- matrix(nrow = N, ncol = nitem_D)
       item_D <- matrix(nrow = nitem_D, ncol = 3)
       initialitem_D <- matrix(nrow = nitem_D, ncol = 3)
@@ -177,44 +176,88 @@ DataGeneration <- function(seed=1, N=2000,
                                 runif(1, min = c_l, max = c_u)), digits = 2)
           initialitem_D[i,] <- c((a_l+a_u)/2,0,0)
         }
+      }
 
+        # item responses for dichotomous items
 
+      for(j in 1:N){
+        p <- P(theta = theta[j], a = item_D[i,1], b = item_D[i,2], c= item_D[i,3])
+        data_D[j,i] <- rbinom(1,1,prob = p)
+      }
+    }
+  } else {
+    if((nitem_D!=0)&(!is.null(nitem_D))){
+      data_D <- matrix(nrow = N, ncol = nitem_D)
+      initialitem_D <- matrix(nrow = nitem_D, ncol = 3)
+      set.seed(seed)
+      for(i in 1:nitem_D){
+        if(model_D[i]==1){
+          initialitem_D[i,] <- c(1,0,0)
+        }else if(model_D[i]==2){
+          initialitem_D[i,] <- c((a_l+a_u)/2,0,0)
+        } else if(model_D[i]==3){
+          initialitem_D[i,] <- c((a_l+a_u)/2,0,0)
+        }
+      }
 
       # item responses for dichotomous items
 
-        for(j in 1:N){
-          p <- P(theta = theta[j], a = item_D[i,1], b = item_D[i,2], c= item_D[i,3])
-          data_D[j,i] <- rbinom(1,1,prob = p)
-        }
+      for(j in 1:N){
+        p <- P(theta = theta[j], a = item_D[i,1], b = item_D[i,2], c= item_D[i,3])
+        data_D[j,i] <- rbinom(1,1,prob = p)
       }
     }
   }
 
 
+
+
   # item parameters for polytomous items
+  if(is.null(item_P)){
+    if((nitem_P!=0)&(!is.null(nitem_P))){
+      data_P <- matrix(nrow = N, ncol = nitem_P)
+      item_P <- matrix(nrow = nitem_P, ncol = 7)
+      initialitem_P <- matrix(nrow = nitem_P, ncol = 7)
+      set.seed(seed)
+      for(i in 1:nitem_P){
+        if(model_P=="GPCM"){
+          item_P[i,1] <- round(runif(1,a_l,a_u), digits = 2)
+          center <- rnorm(1,0,.5)
+          item_P[i,2:(categ[i])] <- sort(rnorm(categ[i]-1,center,.2))
 
-  if(nitem_P!=0){
-    data_P <- matrix(nrow = N, ncol = nitem_P)
-    item_P <- matrix(nrow = nitem_P, ncol = 7)
-    initialitem_P <- matrix(nrow = nitem_P, ncol = 7)
-    set.seed(seed)
-    for(i in 1:nitem_P){
-      if(model_P=="GPCM"){
-        item_P[i,1] <- round(runif(1,a_l,a_u), digits = 2)
-        center <- rnorm(1,0,.5)
-        item_P[i,2:(categ[i])] <- sort(rnorm(categ[i]-1,center,.2))
+          initialitem_P[i,1] <- 1
+          initialitem_P[i,2:(categ[i])] <- 0#(-2:1+.5)/3
+          }
 
-        initialitem_P[i,1] <- 1
-        initialitem_P[i,2:(categ[i])] <- 0#(-2:1+.5)/3
+
+        # item responses for polytomous items
+
+        for(j in 1:N){
+          pp <- P_P(theta = theta[j], a = item_P[i,1], b = item_P[i,-1])
+          pp <- pp[!is.na(pp)]
+          data_P[j,i] <- sample(x = 0:(categ[i]-1),1,prob=pp)
+        }
+      }
+    }
+  } else {
+    if((nitem_P!=0)&(!is.null(nitem_P))){
+      data_P <- matrix(nrow = N, ncol = nitem_P)
+      initialitem_P <- matrix(nrow = nitem_P, ncol = 7)
+      set.seed(seed)
+      for(i in 1:nitem_P){
+        if(model_P=="GPCM"){
+          initialitem_P[i,1] <- 1
+          initialitem_P[i,2:(categ[i])] <- 0#(-2:1+.5)/3
         }
 
 
-      # item responses for polytomous items
+        # item responses for polytomous items
 
-      for(j in 1:N){
-        pp <- P_P(theta = theta[j], a = item_P[i,1], b = item_P[i,-1])
-        pp <- pp[!is.na(pp)]
-        data_P[j,i] <- sample(x = 0:(categ[i]-1),1,prob=pp)
+        for(j in 1:N){
+          pp <- P_P(theta = theta[j], a = item_P[i,1], b = item_P[i,-1])
+          pp <- pp[!is.na(pp)]
+          data_P[j,i] <- sample(x = 0:(categ[i]-1),1,prob=pp)
+        }
       }
     }
   }
